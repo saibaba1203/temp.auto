c634b507d53dbc64b0c23f0cd60be5e320bc123f
sanketdesai <sanketdesai.162001@gmail.com>
2022-05-09 11:54:16 +0100

    my temp auto

---
+++ src/test/java/com/automation/AppTest.java
@@ -0,0 +1,22 @@
+package com.automation;
+
+
+import org.testng.annotations.Test;
+
+import static graphql.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+
+        assertTrue( true );
+    }
+}
